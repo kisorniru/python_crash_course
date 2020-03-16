f8d3489f6146d182949e392a8768ac3f45521594
kisorniru <kisorniru@gmail.com>
2020-03-17 02:11:00 +0600

    teest

---
+++ python_list.py
@@ -0,0 +1,32 @@
+
+
+mix_list = ['honda', 28, 26.1, False, 'audi']
+
+
+print(mix_list)
+
+matrix = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+]
+
+print(matrix)
+
+for x in matrix:
+    for y in x:
+        print(y, end=' ')
+    print()
+
+# List Slicing
+numbers_list = [1, 2, 3, 4, 5]
+print(numbers_list[0:2])
+print(numbers_list[2:-2])
+
+# List Iteration mby for loop
+cars = ['honda', 'toyota', 'audi']
+for car in cars:
+    print(car)
+    if car == 'toyota':
+        print('I love toyota')
